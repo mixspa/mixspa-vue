efe605e5fbd51ae7355fa0cb72b2b6651d823d7b
Cao Xiaoqi <xqcao@thoughtworks.com>
2019-07-28 16:53:50 +0000

    Release v0.1.0

---
+++ lib/AppLink.vue
@@ -0,0 +1,17 @@
+<template>
+    <a href="javascript:void" @click.prevent="onClick"><slot /></a>
+</template>
+
+<script>
+import Mixspa from '@mixspa/core';
+
+export default {
+  props: ['base', 'to'],
+  methods: {
+    onClick(event) {
+      console.log('on click');
+      Mixspa.emitLink(this.base + this.to);
+    }
+  }
+}
+</script>
